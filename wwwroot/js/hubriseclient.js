1980b17ebca3cad5872b8e4500ce9d25e8d40326
truecode112 <truecoder112@gmail.com>
2023-02-28 04:46:46 +0900

    Added logo and actions

--- wwwroot/js/hubriseclient.js
+++ wwwroot/js/hubriseclient.js
@@ -26,20 +26,27 @@ function processOrder(formattedData, rawData) {
             expectTime = new Date();
         }
 
+        var company, type;
+        if (order.new_state.service_type_ref != null) {
+            company = order.new_state.service_type_ref.split("-")[0].toLowerCase();
+            type = order.new_state.service_type_ref.split("-")[1].toLowerCase();
+        }
+
         var orderCard = `<summary class="card mt-2 flex-row" id="${order.order_id}" onClick="viewOrder(event, '${encoded}', '${order.order_id}', '${order.location_id}', '${expectTime}')"> 
-        <div class="ml-3 mr-3 w-10 flex-column d-flex align-items-center justify-content-center">
-            <p class="card-title mt-0 mb-0 font-weight-bold" >${expectTime.getHours()}:${expectTime.getMinutes()}</p> 
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-bicycle" viewBox="0 0 16 16"> 
-                <path d="M4 4.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1v.5h4.14l.386-1.158A.5.5 0 0 1 11 4h1a.5.5 0 0 1 0 1h-.64l-.311.935.807 1.29a3 3 0 1 1-.848.53l-.508-.812-2.076 3.322A.5.5 0 0 1 8 10.5H5.959a3 3 0 1 1-1.815-3.274L5 5.856V5h-.5a.5.5 0 0 1-.5-.5zm1.5 2.443-.508.814c.5.444.85 1.054.967 1.743h1.139L5.5 6.943zM8 9.057 9.598 6.5H6.402L8 9.057zM4.937 9.5a1.997 1.997 0 0 0-.487-.877l-.548.877h1.035zM3.603 8.092A2 2 0 1 0 4.937 10.5H3a.5.5 0 0 1-.424-.765l1.027-1.643zm7.947.53a2 2 0 1 0 .848-.53l1.026 1.643a.5.5 0 1 1-.848.53L11.55 8.623z" /> 
-            </svg> 
-        </div> 
-        <div class=""> 
-            <h6 class="card-title mb-0 mt-1">${order.new_state.customer.address_1 == null || typeof order.new_state.customer.address_1 != "string" ? "No address" : order.new_state.customer.address_1} ${order.new_state.customer.address_2 == null ? "" : order.new_state.customer.address_2}</h6> 
-            <p class="mt-0 mb-0" style="font-size:13px;">${order.new_state.customer.first_name} ${order.new_state.customer.last_name}</p> 
-            <p class="card-text mt-0 mb-1" style="font-size:13px;">${order.new_state.items.length} Product${order.new_state.items.length > 1 ? "s" : ""} (${order.new_state.total})</p> 
-            <input type="hidden" name="order_id" value="${order.id}"/> 
-        </div> 
-    </summary>`;
+            <div class="ml-2 w-10 flex-column d-flex align-items-center justify-content-center">
+                <img src="${company == "ubereat" ? "/imgs/ubereats.png" : ( company == "takeaway" ? "/imgs/takeaway.png" : "/imgs/justeat.png")}" width="48px" height="48px" />
+            </div>
+            <div class="ml-2 mr-3 w-10 flex-column d-flex align-items-center justify-content-center">
+                <p class="card-title mt-0 mb-0 font-weight-bold" >${expectTime.getHours()}:${expectTime.getMinutes()}</p> 
+                <img src="${type == "delivery" ? "/imgs/delivery.png" : "/imgs/collection.png"}" width="36px" height="36px" />
+            </div> 
+            <div class=""> 
+                <h6 class="card-title mb-0 mt-1">${order.new_state.customer.address_1 == null || typeof order.new_state.customer.address_1 != "string" ? "No address" : order.new_state.customer.address_1} ${order.new_state.customer.address_2 == null ? "" : order.new_state.customer.address_2}</h6> 
+                <p class="mt-0 mb-0" style="font-size:13px;">${order.new_state.customer.first_name} ${order.new_state.customer.last_name}</p> 
+                <p class="card-text mt-0 mb-1" style="font-size:13px;">${order.new_state.items.length} Product${order.new_state.items.length > 1 ? "s" : ""} (${order.new_state.total})</p> 
+                <input type="hidden" name="order_id" value="${order.id}"/> 
+            </div> 
+        </summary>`;
     }
     catch (err) {
         console.log("err", err);
